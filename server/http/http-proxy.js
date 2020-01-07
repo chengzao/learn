ce97f495beb386094f8d9047b41ecaa49ef68396
chengzao <czhlink@163.com>
2020-01-07 18:43:10 +0800

    update

--- server/http/http-proxy.js
+++ server/http/http-proxy.js
@@ -1,18 +1,18 @@
-let express = require('express');
-let axios = require('axios');
+let express = require('express')
+let axios = require('axios')
 
-let app = express();
+let app = express()
 
-app.get('/api',function(req,res){
-    async function fn(){
-        let data = await axios.get(`https://api.github.com/users/chengzao`);
-        let res2  = data.data;
-        res.send( res2 );
-    }
-    fn();
+app.get('/api', function(req, res) {
+  async function fn() {
+    let data = await axios.get(`https://api.github.com/users/chengzao`)
+    let res2 = data.data
+    res.send(res2)
+  }
+  fn()
 })
 
-var server = app.listen(8080,function(){
-    var host = server.address();
-    console.log(`server is up on port ${host.port}`);
+var server = app.listen(8080, function() {
+  var host = server.address()
+  console.log(`server is up on port ${host.port}`)
 })
--- server/http/tojs.js
+++
@@ -1,4 +0,0 @@
-//将数据通过 JS 进行直接加载
-
-var data = 'b site data';
-
