0ec205d41ee0af8e831871e209395a5118bbd04c
chengzao <czhlink@163.com>
2020-01-07 17:42:04 +0800

    fix

--- ECMAScript/asyn/asyn-readFile2.js
+++
@@ -1,9 +0,0 @@
-//嵌套回调，读一个文件后输出，再读另一个文件，注意文件是有序被输出的，先text1.txt后text2.txt
-var fs = require('fs')
-
-fs.readFile('./text1.txt', 'utf8', function(err, data) {
-  console.log('text1 file content: ' + data)
-  fs.readFile('./text2.txt', 'utf8', function(err, data) {
-    console.log('text2 file content: ' + data)
-  })
-})
--- ECMAScript/class/mixins.js
+++
@@ -1,5 +0,0 @@
-export function mixins(...list) {
-    return function (target) {
-      Object.assign(target.prototype, ...list)
-    }
-}
